14fcafe9aa8486a33780ce129106969d36d36263
filzrev <103790468+filzrev@users.noreply.github.com>
2026-04-09 09:02:27 +0900

    refactor and disable apphost

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -4,6 +4,7 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        await DisassemblyDiagnoserTests.RunDumpThisProcessWithDotNetDumpAsync(CancellationToken.None).ConfigureAwait(false);
+        await DisassemblyDiagnoserTests.RunDumpThisProcessAsync(CancellationToken.None).ConfigureAwait(false);
+        // await DisassemblyDiagnoserTests.RunDumpThisProcessWithDotNetDumpAsync(CancellationToken.None).ConfigureAwait(false);
     }
 }
